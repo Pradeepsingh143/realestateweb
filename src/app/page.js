990939db4cb38e705250e5d7d7e77d23d54468ff
Pradeep singh <singhpardeep585@gmail.com>
2023-07-20 04:09:18 +0530

    fixing error

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import homeimage from "../../public/assests/home.jpg";
 import fastcash from "../../public/assests/fastcash.png";
 import personalizedSeller from "../../public/assests/personalized.png";
 import consultations from "../../public/assests/Consulting.png";
-import investment from "../../public/assests/startup.png";
+import investment from "../../public/assests/invest.png";
 import aboutImage from "../../public/assests/about-image.jpg";
 import group from "../../public/assests/group.svg"
 import Link from "next/link";
